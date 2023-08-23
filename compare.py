bb3819040e22636768faa9fc839297eb9f1969fa
Carlos Adrian Denegri Martinez <cadenegr@c4c1c3.42berlin.de>
2023-08-23 16:29:44 +0200

    pushing tasks

---
+++ compare.py
@@ -0,0 +1,42 @@
+#Task 1
+# Your task is to write a program which asks the user three times about two integer numbers to compare.
+# Hint: Use while loop to perform the same operation more than once!
+# Use both comparison and logical operators, for example use logical comparison between two or more comparion operators:
+
+# print("Task1"")
+# i = 0
+# while i < 3:
+# 	num1 = int(input("Enter first number: "))
+# 	num2 = int(input("Enter second number: "))
+# 	if (num1 >= 999) and (num2 >= 999):
+# 		size = 1
+# 	else:
+# 		size = 0
+# 	if num1 != num2:
+# 		print("Numbers are not equal")
+# 	else:
+# 		print("Numbers are equal")
+# 	if num1 > num2:
+# 		print("The first number is greater than the second number")
+# 	else:
+# 		print("The first number is lesser to the second number")
+# 	if size == 1:
+# 		print("Both numbers are large")
+# 		print("Both numbers are set to True")
+# 	else:
+# 		print("Both numbers are small")
+# 		print("Both numbers are set to False")
+# 	i += 1
+
+#Task 2
+
+# print("Task2")
+# print("List of months: January, February, March, April, May, June, July, August, September, October, November, December")
+# month = str(input("Enter month: "))
+
+# if (month == "January") or (month =="March") or (month == "May") or (month == "July") or (month == "August") or (month == "October") or (month == "December"):
+# 	print(f"{month} has 31 days")
+# elif month == "February":
+# 	print(f"{month} has 28 or 29 days")
+# else:
+# 	print(f"{month} has 30 days")
